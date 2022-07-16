d689d20279796071d3c450e94e3c6f92c7677e83
lucioana <nikita.suslov.03@mail.ru>
2022-07-16 22:48:14 +0300

    nearly to work

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,27 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <time.h>
 
 int check_cell(int **field, int rows, int columns, int i, int j);
-void update_field (int **field, int rows, int columns);
+int update_field (int **field, int rows, int columns);
 void print_field(int **field, int rows, int columns);
 void free_m(int **a, int rows);
-int input(int **buffer, int rows, int columns);
+int input_field(int **buffer, int rows, int columns);
+void game(int **field, int rows, int columns);
+int ** matrix_init(int rows, int columns);
+void to_not_canon();
+void to_canon();
+void output(int **buffer, int rows, int columns);
+
+struct termios saved_attributes;
 
 int main () {
-    
+    int **field;
+    int rows, columns;
+
+    rows = 25;
+    columns = 80;
+
+    field = matrix_init(rows, columns);
+
+
+    if (field) {
+        to_not_canon();
+        game(field, rows, columns);
+        to_canon();
+    }
 }
 
-void choose_menu(int **field){
-    
+void game(int **field, int rows, int columns) {
+    int input;
+    double step, speed;
+    clock_t start;
+
+    step = 0.1;
+    speed = 5;
+    start = clock();
+
+    while (1) {
+
+        if ((speed != -1.0125 && (double) (clock() - start) / CLOCKS_PER_SEC >= speed * step) || (speed == -1.0125 && (input == 61 || input == 43))) {
+            if (update_field(field, rows, columns) == 1){
+                print_field(field, rows, columns);
+                printf("end of game\n");
+                break;
+            }
+                
+            print_field(field, rows, columns);
+            start = clock();
+        }
+
+        input = getchar();
+
+        if (47 < input && input < 58)
+            speed = (double) (1.1125) * (input - 48) - 1.0125;
+    }
 }
 
-void game (int ** start_statement) {
-    int height = 25, wight = 80;
-    update_field(start_statement, height, wight);
-    print_field(start_statement, height, wight);
+void to_not_canon() {
+    struct termios termios_attr;
+    int flags;
+    flags = fcntl(0, F_GETFL, 0);
+    flags |= O_NONBLOCK;
+    fcntl(0, F_SETFL, flags);
+    tcgetattr(0, &saved_attributes);
+    atexit(to_canon);
+    tcgetattr(0, &termios_attr);
+    termios_attr.c_lflag &= ~(ICANON|ECHO);
+    termios_attr.c_cc[VMIN] = 1;
+    termios_attr.c_cc[VTIME] = 0;
+    tcsetattr(0, TCSAFLUSH, &termios_attr);
 }
 
-void update_field(int **field, int rows, int columns) {
+void to_canon() {
+    tcsetattr(0, TCSAFLUSH, &saved_attributes);
+}
+
+int update_field(int **field, int rows, int columns) {
+    int flag = 1;
     int ** new_field = calloc(rows, sizeof(int *));
 
     for (int i = 0; i < rows; i++)
@@ -30,6 +92,8 @@ void update_field(int **field, int rows, int columns) {
     for (int i = 0; i < rows; i++) {
         for (int j =  0; j < columns; j++) {
             new_field[i][j] = check_cell(field, rows, columns, i, j);
+            if (new_field[i][j] == 1)
+                flag = 0;
         }
     }
 
@@ -39,39 +103,85 @@ void update_field(int **field, int rows, int columns) {
     }
 
     free(new_field);
+
+    return flag;
 }
 
-int mode(int coordinate, int size){
+int module(int coordinate, int size){
+    if (coordinate >= 0) {
+        coordinate = coordinate % size;
+    } else {
+        coordinate = size + (coordinate % size);
+    }
+    return coordinate;
 
 }
 
 int check_cell(int **field, int rows, int columns, int i, int j) {
+    int neighbour, ans;
 
-}//
+    neighbour = 0;
+
+    if (field[module(i - 1, rows)][j] == 1) {
+        neighbour += 1;
+    }
+    if (field[module(i + 1, rows)][j] == 1) {
+        neighbour += 1;
+    }
+    if (field[module(i + 1, rows)][module(j + 1, columns)] == 1) {
+        neighbour += 1;
+    }
+    if (field[module(i - 1, rows)][module(j - 1, columns)] == 1) {
+        neighbour += 1;
+    }
+    if (field[module(i - 1, rows)][module(j + 1, columns)] == 1) {
+        neighbour += 1;
+    }
+    if (field[module(i + 1, rows)][module(j - 1, columns)] == 1) {
+        neighbour += 1;
+    }
+    if (field[i][module(j + 1, columns)] == 1) {
+        neighbour += 1;
+    }
+    if (field[i][module(j - 1, columns)] == 1) {
+        neighbour += 1;
+    }
+
+
+    if ((field[i][j] == 1 && neighbour == 2) || neighbour == 3)
+        ans = 1;
+    else
+        ans = 0;
+
+    return ans;
+}
 
 void print_field(int **field, int rows, int columns) {
+    printf("\033[2J");
 
-}//
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++){
+            if (field[i][j] == 1)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+}
 
-int ** matrix_init(int *rows, int *columns) {
+int ** matrix_init(int rows, int columns) {
     int **pointers_matrix = NULL;
 
-    if (scanf("%d", rows) == 0 || *rows <= 0) {
+    pointers_matrix = malloc(rows * sizeof(int *));
+
+    for (int i = 0; i < rows; i++)
+        pointers_matrix[i] = malloc(columns * sizeof(int));
+
+    if (input_field(pointers_matrix, rows, columns) == 1) {
         printf("n/a");
-    } else if (scanf("%d", columns) == 0 || *columns <= 0) {
-        printf("n/a");
-    } else {
-        pointers_matrix = malloc(*rows * sizeof(int *));
-
-        for (int i = 0; i < *rows; i++)
-            pointers_matrix[i] = malloc(*columns * sizeof(int));
-
-        if (input(pointers_matrix, *rows, *columns) == 1) {
-            printf("n/a");
-
-            free_m(pointers_matrix, *rows);
-            pointers_matrix = NULL;
-        }
+        free_m(pointers_matrix, rows);
+        pointers_matrix = NULL;
     }
 
     return pointers_matrix;
@@ -84,7 +194,7 @@ void free_m(int **a, int rows) {
     free(a);
 }
 
-int input(int **buffer, int rows, int columns) {
+int input_field(int **buffer, int rows, int columns) {
     int flag, trash;
 
     flag = 0;
@@ -113,74 +223,18 @@ int input(int **buffer, int rows, int columns) {
     return flag;
 }
 
-void help(const char *mod){
-    switch (mod[0]) {
-        case 's':
-            switch (mod[1]) {
-                case 'm':
-                    printf("0. Close.\n"
-                           "1. Help.\n"
-                           "2. Input field.\n"
-                           "3. Chose field.\n");
-                    break;
-                case 'h':
-                    printf("0. Close this program.\n"
-                           "1. Print this text.\n"
-                           "2. Allow you to input field by yourself.\n"
-                           "3. Move to choose menu, where you can choose start field.\n"
-                           "You can't start game before input/choose field.\n");
-                    break;
-                default:
-                    printf("Wrong command, try again. Enter \"1\" for get more info about commands\n");
-            }
-            break;
-        case 'm':
-            switch (mod[1]) {
-                case 'm':
-                    printf("0. Close.\n"
-                           "1. Help.\n"
-                           "2. Input field.\n"
-                           "3. Chose field.\n"
-                           "4. Start.\n");
-                    break;
-                case 'h':
-                    printf("0. Close this program.\n"
-                           "1. Print this text.\n"
-                           "2. Allow you to input field again by yourself.\n"
-                           "\tYou need enter 25 rows with 80 integers inside.\n"
-                           "\tIf you mistake field won't update.\n"
-                           "3. Move to choose menu, where you can choose again start field.\n"
-                           "4. Start the game.\n");
-                    break;
-                default:
-                    printf("Wrong command, try again. Enter \"1\" for get more info about commands\n");
-            }
-            break;
-        case 'c':
-            switch (mod[1]) {
-                case 'm':
-                    printf("0. Back.\n"
-                           "1. Help.\n"
-                           "2. field_1.\n"
-                           "3. field_2.\n"
-                           "4. field_3.\n");
-                    break;
-                case 'h':
-                    printf("0. Move to previous menu.\n"
-                           "1. Print this text.\n"
-                           "2. Make start field like (description field_1)\n"
-                           "3. Make start field like (description field_2)\n"
-                           "4. Make start field like (description field_3)\n");
-                    break;
-                default:
-                    printf("Wrong command, try again. Enter \"1\" for get more info about commands\n");
-            }
-            break;
-        default:
-            printf("Wrong command, try again. Enter \"1\" for get more info about commands\n");
-    }
-}
+void output(int **buffer, int rows, int columns) {
+    for (int i = 0; i < rows - 1; i++) {
+        printf("%d", buffer[i][0]);
 
-void help_for_choose_menu(){
-    
+        for (int j = 1; j < columns; j++)
+            printf(" %d", buffer[i][j]);
+
+        printf("\n");
+    }
+
+    printf("%d", buffer[rows - 1][0]);
+
+    for (int j = 1; j < columns; j++)
+        printf(" %d", buffer[rows - 1][j]);
 }
